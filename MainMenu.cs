7232fb0266e1856d132079f9c2c56c7ba9706deb
Sergey <cepblvl@gmail.com>
2020-06-14 07:22:07 +0300

    menu_template

--- MainMenu.cs
+++ MainMenu.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public GameObject SettingsMenu;
+    public void Settings(){
+        SettingsMenu.SetActive(true);
+    }
+    public void ExitSettings(){
+        SettingsMenu.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
