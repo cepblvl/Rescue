ad7b53ea575b69a30a3d768a533ef560f17ecd83
Sergey <cepblvl@gmail.com>
2020-06-23 12:19:48 +0300

    add_model_simulation

--- Region.cs
+++ Region.cs
@@ -8,29 +8,43 @@ public class Region : MonoBehaviour
     private GameContoller GameScript;
 
     //Характеристики
-    public int Population;
-    public int Sick=0;
-    public int Dead;
-    public int Health;
-    public int g;  //дни, за которые происходит излечение от болезни
+    public float N=10000;//Population
+    public float I=1;//Sick
+    public float D=0;//Dead
+    public float R=0; //Recovering
+    public float C=5;//Infect
+    public float S;//Health
+    public float g=100;  //дни, за которые происходит излечение от болезни
     public float gamma; //величина, обратная среднему инкубационному периоду заболевания
     public float beta; //контролирующий уровень передачи заболевания при воздействии
     public float alpha; //уровень смертности
-
+    public float St;//здоровые в предыдущий день
+    public float It;//больные в предыдущий день
+    
+          
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        S=N-I-D-R;
+        gamma=1f/g;
+        beta=gamma*C;
+        alpha=3*gamma/100;
         GameScript = GameControl.GetComponent<GameContoller>();
         StartCoroutine("SickPlusPlus");
     }
     IEnumerator SickPlusPlus()
     {
         for (; ; )
-        {
-            Sick++;
+        {   
+            St=S;
+            It=I;
+            S=S+(-(beta*It*St)/N);
+            I=I+beta*It*St/N-(gamma+alpha)*It;
+            R=R+gamma*It;
+            D=D+alpha*It;
             yield return new WaitForSeconds(1f/GameScript.time);
         }
     }
